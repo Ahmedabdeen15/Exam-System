fbb9d7a905c3f60cdb3d153e783e296274b46919
Ahmed Waleed <ahmedwalleed15@gmail.com>
2025-05-11 19:41:48 +0300

    Add dark theme toggle functionality and update theme icon in start page

--- js/start.js
+++ js/start.js
@@ -1,58 +1,81 @@
+var darkTheme;
 document.addEventListener("DOMContentLoaded", () => {
-    const isLoggedIn = localStorage.getItem("isLoggedIn");
-    if (isLoggedIn) {
-        const loader = document.getElementById("formOverlay");
-        loader.classList.add("hidden");
+  setTheme();
+  const isLoggedIn = localStorage.getItem("isLoggedIn");
+  if (isLoggedIn) {
+    const loader = document.getElementById("formOverlay");
+    loader.classList.add("hidden");
+  }
+
+  document.body.classList.add("st-light-mode");
+
+  const themeBtn = document.getElementById("st-theme-btn");
+  themeBtn.addEventListener("click", () => {
+    darkTheme = !darkTheme;
+    localStorage.setItem("darkTheme", darkTheme);
+    setTheme();
+  });
+
+  const topicSpan = document.querySelector(".st-left-content span");
+
+  const topicButtons = document.querySelectorAll(".st-exam-topic");
+  topicButtons.forEach((btn) => {
+    btn.addEventListener("click", () => {
+      topicButtons.forEach((b) => b.classList.remove("st-active"));
+      btn.classList.add("st-active");
+      topicSpan.innerText = `${btn.innerText} Exam!`;
+    });
+  });
+
+  const questionButtons = document.querySelectorAll(".st-question-option");
+  questionButtons.forEach((btn) => {
+    btn.addEventListener("click", () => {
+      questionButtons.forEach((b) => b.classList.remove("st-active"));
+      btn.classList.add("st-active");
+    });
+  });
+
+  const startBtn = document.querySelector(".st-start-exam-btn");
+  startBtn.addEventListener("click", () => {
+    let selectedTopic = document.querySelector(
+      ".st-exam-topic.st-active"
+    ).innerText;
+    const selectedCount = document.querySelector(
+      ".st-question-option.st-active"
+    ).innerText;
+
+    if (selectedTopic === "C#") {
+      selectedTopic = "C_Sharp";
     }
+    localStorage.setItem("examTopic", selectedTopic);
+    localStorage.setItem("questionCount", selectedCount);
 
-    document.body.classList.add("st-light-mode");
-
-    const themeBtn = document.getElementById("st-theme-btn");
-    themeBtn.addEventListener("click", () => {
-        document.body.classList.toggle("st-dark-mode");
-        document.body.classList.toggle("st-light-mode");
-
-        const img = themeBtn.querySelector("img");
-        img.src = document.body.classList.contains("st-dark-mode")
-            ? "assets/icons/icon-sun.svg"
-            : "assets/icons/icon-dark.svg";
-    });
-
-    const topicSpan = document.querySelector(".st-left-content span");
-
-    const topicButtons = document.querySelectorAll(".st-exam-topic");
-    topicButtons.forEach((btn) => {
-        btn.addEventListener("click", () => {
-            topicButtons.forEach(b => b.classList.remove("st-active"));
-            btn.classList.add("st-active");
-            topicSpan.innerText = `${btn.innerText} Exam!`;
-        });
-    });
-
-    const questionButtons = document.querySelectorAll(".st-question-option");
-    questionButtons.forEach((btn) => {
-        btn.addEventListener("click", () => {
-            questionButtons.forEach(b => b.classList.remove("st-active"));
-            btn.classList.add("st-active");
-        });
-    });
-
-    const startBtn = document.querySelector(".st-start-exam-btn");
-    startBtn.addEventListener("click", () => {
-        let selectedTopic = document.querySelector(".st-exam-topic.st-active").innerText;
-        const selectedCount = document.querySelector(".st-question-option.st-active").innerText;
-
-        if (selectedTopic === "C#") {
-            selectedTopic = "C_Sharp";
-        }
-        localStorage.setItem("examTopic", selectedTopic);
-        localStorage.setItem("questionCount", selectedCount);
-
-        window.location.replace("examPage");
-    });
+    window.location.replace("examPage");
+  });
 });
 
 function logout() {
-    localStorage.removeItem('isLoggedIn');
-    window.location.replace('login');
+  localStorage.removeItem("isLoggedIn");
+  window.location.replace("login");
+}
+function setTheme() {
+  var storedTheme = localStorage.getItem("darkTheme");
+  if (darkTheme === null) {
+    darkTheme = false;
+    localStorage.setItem("darkTheme", darkTheme);
+  }
+  {
+    darkTheme = storedTheme === "true";
+  }
+  const img = document.getElementById("themeIcon");
+  img.src = darkTheme
+    ? "assets/icons/icon-sun.svg"
+    : "assets/icons/icon-dark.svg";
+  if (darkTheme) {
+    document.body.classList.add("st-dark-mode");
+    document.body.classList.remove("st-light-mode");
+  } else {
+    document.body.classList.add("st-light-mode");
+    document.body.classList.remove("st-dark-mode");
+  }
 }
